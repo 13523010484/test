a22941889365b55294dd53040fc512e9e1d67546
jiaqh <jiaqiuhua@ysstech>
2022-07-21 13:16:07 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,10 @@
 
 # 当发生错误时中止脚本
 set -e
-
 # 构建
 npm run build
-git add -A
+git add .
 git commit -m "deploy"
-# 部署到 https://<USERNAME>.github.io/<REPO>
+git push origin main
 git subtree push --prefix dist origin gh-pages
-
 cd -
