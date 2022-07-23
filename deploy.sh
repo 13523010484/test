82bb376c0cd1d52bacbc849a51b0d0b552d0fe49
jiaqh <2286592704@qq.com>
2022-07-23 22:34:26 +0800

    修改deploy的推送分支

--- deploy.sh
+++ deploy.sh
@@ -26,6 +26,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:13523010484/test.git master:gh-pages
+git push -f git@github.com:13523010484/test.git main:gh-pages
 
 cd -
