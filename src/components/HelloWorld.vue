9fa5b831c2e59d4859a13d2a3dd414f26bf5df7b
jiaqh <jiaqiuhua@ysstech>
2022-07-25 16:46:09 +0800

    修改HelloWorld.vue组件，验证是否自动构建

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>当前日期::{{ new Date().toLocaleString() }}</p>
+    <p>当前日期1::{{ new Date().toLocaleString() }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
